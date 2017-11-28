e21ac11bfc4f9cf321d2b68d246d17f092f1066b
dorukhan93 <dbasoglu93@gmail.com>
2017-11-28 13:38:30 -0500

    Call Class
    added Call Class
    -equimentType
    -serviceType
    -timeIn

---
+++ src/simModel/Call.java
@@ -0,0 +1,16 @@
+package simModel;
+
+public class Call {
+
+	// Denotes the type of Equipment that must be repaired. 
+		enum equipmentType { E1000, E2000, E3000, E4000};
+		equipmentType uType1;  // Type of equipment
+		
+		// Denotes the quality of service provided to the customer. Directly affects the response time.
+		enum serviceType { BASIC, PREMIUM};
+		serviceType uType2;  // Type of service
+		
+		// Time that the call was entered into the system.
+		double timeIn;	
+	
+}
