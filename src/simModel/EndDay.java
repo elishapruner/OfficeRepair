3741b1aab967bde39e1b12644f41792e18838ad1
ArenLavoie <aren.lavoie@gmail.com>
2017-12-01 19:49:34 -0500

    Update EndDay.java

--- src/simModel/EndDay.java
+++ src/simModel/EndDay.java
@@ -1,24 +1,56 @@
 package simModel;
 
+import static simModel.Call.serviceType.PREMIUM;
 import simulationModelling.ScheduledAction;
 
 public class EndDay {
+    
+        protected static boolean precondition(OfficeRepair simModel)
+                {
+                        boolean returnValue = false;
 
-	class Call_Recieved extends ScheduledAction 
-	{
-		OfficeRepair model;
-		public Call_Recieved (OfficeRepair model) {this.model=model;}
 
-		public double timeSequence()
-		{
-			return 0;				
-		}
+                    if (((int)simModel.getClock())%1440 == 0 )   //	rEmployees[0][1] this needs to be fixed
+                        returnValue = true;
 
-		public void actionEvent() 
-		{
-			
-		}
-	}
+                        return(returnValue);
+                }
+
+	OfficeRepair model;
+        public EndDay(OfficeRepair model) { this.model = model; }
+
+        public double timeSequence()
+        {
+                return 0;				
+        }
+
+
+
+        public void actionEvent() 
+        {
+                for(Employee e : model.rEmployees[model.constants.Employee_T12]){
+                    if(e.Status == Employee.StatusValues.SERVICING_CALL){
+                        if(e.call.uType2 == PREMIUM){
+                            //ToDo: end the call
+
+                            //Set employee status to normal
+                            e.call = null;
+                            e.Status = Employee.StatusValues.READY_FOR_CALL;
+                        }
+                    }
+                }
+                for(Employee e : model.rEmployees[model.constants.Employee_ALL]){
+                    if(e.Status == Employee.StatusValues.SERVICING_CALL){
+                        if(e.call.uType2 == PREMIUM){
+                            //ToDo: end the call
+
+                            //Set employee status to normal
+                            e.call = null;
+                            e.Status = Employee.StatusValues.READY_FOR_CALL;
+                        }
+                    }
+                }
+        }
 	
 }
 
