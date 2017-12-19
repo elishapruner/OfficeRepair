dfd55a0c76c248e1e1038a2198962353d9ab31ff
Elisha Pruner <elishapruner@gmail.com>
2017-12-18 22:33:52 -0500

    added another experiment

---
+++ src/Experiment3.java
@@ -0,0 +1,47 @@
+
+// File: Experiment.java
+// Description:
+
+import cern.jet.random.engine.*;
+import simModel.*;
+
+// Main Method: Experiments
+//
+public class Experiment3 {
+	public static void main(String[] args) {
+		int NUMRUNS = 6;
+		double startTime = 0.0;
+		Seeds[] sds = new Seeds[NUMRUNS];
+		OfficeRepair officeRepair; // Simulation object
+
+		// Lets get a set of uncorrelated seeds
+		RandomSeedGenerator rsg = new RandomSeedGenerator();
+		for (int i = 0; i < NUMRUNS; i++)
+			sds[i] = new Seeds(rsg);
+		
+		int initNumEmpT12 = 5;
+		int initNumEmpAll = 10;
+		double satisfaction = 0.85;
+
+		//double minSimTime = 60;
+		double minSimTime = 1 ;
+		
+
+		
+		int[][] runOutput = new int[NUMRUNS][2];
+		
+		for (int i = 0; i < NUMRUNS; i++) {
+			officeRepair = new OfficeRepair(startTime, sds[i], true, initNumEmpT12, initNumEmpAll, satisfaction, minSimTime);
+			officeRepair.runSimulation();
+			runOutput[i][0] = officeRepair.numEmployeesT12;
+			runOutput[i][1] = officeRepair.numEmployeesALL;
+			System.out.println("Terminated " + (i + 1) + " cases");
+		}
+		
+		for (int i = 0; i < NUMRUNS; i++) {
+			System.out.println("(" + (i+1) + ") \tnumEmployeesT12: " + runOutput[i][0] + ", numEmployeesAll: " + runOutput[i][1]);
+		}
+		
+
+	}
+}
